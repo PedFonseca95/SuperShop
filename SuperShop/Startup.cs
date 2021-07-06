0ebf230334eb1e645c0462329a543da43e18f9aa
PedFonseca95 <pedro.ricardo.fonseca1995@gmail.com>
2021-07-06 13:17:46 +0100

    Generic Repository

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -26,7 +26,7 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>(); // Objeto é criado, utilizado uma vez e deixa de existir após a utilização
-            services.AddScoped<IRepository, Repository>(); // Objeto é criado e utilizado durante a execução do programa. Caso carreguemos nos products novamente, ele apaga este objeto e cria um novo
+            services.AddScoped<IProductRepository, ProductRepository>(); // Objeto é criado e utilizado durante a execução do programa. Caso carreguemos nos products novamente, ele apaga este objeto e cria um novo
 
             services.AddControllersWithViews();
         }
