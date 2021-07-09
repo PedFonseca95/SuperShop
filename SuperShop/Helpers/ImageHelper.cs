a43efebbc86ab58e6142ef235a8a9a4b593719c2
PedFonseca95 <pedro.ricardo.fonseca1995@gmail.com>
2021-07-09 19:02:06 +0100

    ImageHelper and ConverterHelper

---
+++ SuperShop/Helpers/ImageHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Helpers
+{
+    public class ImageHelper : IImageHelper
+    {
+        public async Task<string> UploadImageAsync(IFormFile imageFile, string folder)
+        {
+            string guid = Guid.NewGuid().ToString(); // Para salvar as imagens com um nome aleatório evitando que sejam salvas imagens com nomes iguais
+            string file = $"{guid}.jpg";
+
+            string path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                $"wwwroot\\images\\{folder}",
+                file);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(stream);
+            }
+
+            return $"~/images/{folder}/{file}";
+        }
+    }
+}
