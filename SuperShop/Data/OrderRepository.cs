f64187efdd8327bad77e27632dbfa903cc3d58bb
Pedro Fonseca <pedro.ricardo.fonseca1995@gmail.com>
2021-09-04 01:37:40 +0100

    Add items on detail temp

--- SuperShop/Data/OrderRepository.cs
+++ SuperShop/Data/OrderRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SuperShop.Data.Entities;
 using SuperShop.Helpers;
+using SuperShop.Models;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,50 @@ namespace SuperShop.Data
         {
             _context = context;
             _userHelper = userHelper;
+        }
+
+        public async Task AddItemToOrderAsync(AddItemViewModel model, string userName)
+        {
+            var user = await _userHelper.GetUserByEmailAsync(userName); // Verificar se o user existe
+
+            if (user == null) // Se não existir
+            {
+                return;
+            }
+
+            var product = await _context.Products.FindAsync(model.ProductId); // Procurar o produto
+
+            if (product == null)
+            {
+                return;
+            }
+
+            // Depois de se confirmar que os user e produto existem, vai buscar o que já existe na orderDetailTemp (para nao adicionar itens repetidos)
+            var orderDetailTemp = await _context.OrderDetailTemps
+                .Where(odt => odt.User == user && odt.Product == product)
+                .FirstOrDefaultAsync();
+
+            if (orderDetailTemp == null) // Se não houver nenhum item adicionado ainda
+            {
+                // Vamos criá-lo
+                orderDetailTemp = new OrderDetailTemp
+                {
+                    Price = product.Price,
+                    Product = product,
+                    Quantity = model.Quantity,
+                    User = user,
+                };
+
+                _context.OrderDetailTemps.Add(orderDetailTemp);
+            }
+            else
+            {
+                // Caso já existam itens adicionados
+                orderDetailTemp.Quantity += model.Quantity; // modifica o valor do produto
+                _context.OrderDetailTemps.Update(orderDetailTemp); // atualiza a quantidade do produto em questao na orderdetailtemp
+            }
+
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IQueryable<OrderDetailTemp>> GetDetailTempsAsync(string userName)
@@ -54,6 +99,24 @@ namespace SuperShop.Data
                 .ThenInclude(i => i.Product)
                 .Where(o => o.User == user) // Vai buscar as orders criadas por este user
                 .OrderByDescending(o => o.OrderDate);
+        }
+
+        public async Task ModifyOrderDetailTempQuantityAsync(int id, double quantity)
+        {
+            var orderDetailTemp = await _context.OrderDetailTemps.FindAsync(id);
+
+            if (orderDetailTemp == null) // Se não existir
+            {
+                return;
+            }
+
+            // Caso exista
+            orderDetailTemp.Quantity += quantity;
+            if (orderDetailTemp.Quantity > 0) // E a quantidade do produto a adicionar for superior a 0
+            {
+                _context.OrderDetailTemps.Add(orderDetailTemp); // Adiciona o produto à orderdetailtemp
+                await _context.SaveChangesAsync(); // guardar as alterações
+            }
         }
     }
 }
